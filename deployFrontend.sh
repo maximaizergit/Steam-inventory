4f7edc9e31a63aa54b8ae38af5163a46d3a62107
Evgeniy Soroka <eugene.soroka@onilab.com>
2023-07-27 22:40:46 +0300

    Update deployFrontend

--- deployFrontend.sh
+++ deployFrontend.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+NPM_PATH=/root/.nvm/versions/node/v18.17.0/bin/npm
 
 echo "Deployment started ..."
 if [ -f "pidfile" ] && ps -p "$(cat pidfile)" > /dev/null 2>&1; then
@@ -9,4 +10,5 @@ fi
 # Install dependencies and start the React application
 npm install
 npm start &
+
 echo "Deployment finished!"
