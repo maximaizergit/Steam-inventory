ae86f508bc29a36cdadba65b5043438839e85edd
Evgeniy Soroka <eugene.soroka@onilab.com>
2023-07-27 23:18:57 +0300

    Update deployFrontend

--- deployFrontend.sh
+++ deployFrontend.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 set -e
-cd /var/www/html/frontend
-# Load nvm
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
@@ -10,11 +8,7 @@ if [ -f "pidfile" ] && ps -p "$(cat pidfile)" > /dev/null 2>&1; then
   npm stop
 fi
 
-# Install dependencies and start the React application in the background using nohup
+# Install dependencies and start the React application
 npm install
 
-nohup npm start > npm_start_log.txt 2>&1 &
-
-echo $! > pidfile
 echo "Deployment finished!"
-exit 0
