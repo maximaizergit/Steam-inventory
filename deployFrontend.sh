42da460875456318b76ffc79cc83b3926a333f34
Evgeniy Soroka <eugene.soroka@onilab.com>
2023-07-27 17:11:55 +0300

    Update deployFrontend

--- deployFrontend.sh
+++ deployFrontend.sh
@@ -3,24 +3,31 @@ set -e
 
 echo "Deployment started ..."
 
+# Остановить сеанс 'react', если он уже запущен
 screen -X -S react quit || true
 
 echo "Screen react stopped"
 
+# Запустить сеанс 'react' в фоновом режиме
 screen -d -m -S react
 
+echo "Screen react started"
+
+# Войти в сеанс 'react'
 screen -r react
 
-echo "Screen react stated"
+echo "Entered screen react"
 
 cd /var/www/html/frontend/
 
+# Установить зависимости с помощью npm
 npm install
 
-echo "Start install npm install"
+echo "npm install completed"
 
+# Запустить приложение с помощью npm
 npm start
 
-echo "Npm started"
+echo "npm started"
 
 echo "Deployment finished!"
